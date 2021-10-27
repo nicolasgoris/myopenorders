3594ba016e6dc61c9e7c04a2cafb63f26c08bfdb
Nicolas Goris <nicolas.goris@flexso.com>
2021-10-27 15:50:20 +0200

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/flexso/portal/myorders/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.flexso.portal.myorders.controller.MainView", {});
+});

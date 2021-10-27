a5d13b2577e0504ce4264cb052dfa042ceed60be
Nicolas Goris <nicolas.goris@flexso.com>
2021-10-27 17:03:34 +0200

    Creation of My orders

--- uimodule/webapp/Component.js
+++ uimodule/webapp/Component.js
@@ -1,28 +1,7 @@
-sap.ui.define(
-    ["sap/ui/core/UIComponent", "sap/ui/Device", "com/flexso/portal/myorders/model/models"],
-    function (UIComponent, Device, models) {
-        "use strict";
-
-        return UIComponent.extend("com.flexso.portal.myorders.Component", {
-            metadata: {
-                manifest: "json"
-            },
-
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
-
-                // enable routing
-                this.getRouter().initialize();
-
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
-);
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("com.flexso.portal.myorders.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
--- uimodule/webapp/controller/MainView.controller.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define(["com/flexso/portal/myorders/controller/BaseController"], function (Controller) {
-    "use strict";
-
-    return Controller.extend("com.flexso.portal.myorders.controller.MainView", {});
-});
